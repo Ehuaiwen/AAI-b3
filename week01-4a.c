df9080ff1517bcd25b1603bee11e29a9f952d83d
Ehuaiwen <f1317510@gmail.com>
2025-09-10 11:55:35 +0800

    week01

---
+++ week01-4a.c
@@ -0,0 +1,12 @@
+//week 01-4.cpp
+#include <stdio.h>
+int main ()
+{
+	int a,b ;
+	scanf("%d %d",&a, &b);
+	int ans = 0;
+	for (int i=a ; i<=b ;i++){
+		if (i%3==0) ans += i;
+	}
+	printf ("%d", ans);
+}
