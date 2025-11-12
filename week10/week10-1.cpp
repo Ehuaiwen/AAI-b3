c634e483fe09bfdd6f64a9dd373590fb2327c868
Ehuaiwen <f1317510@gmail.com>
2025-11-12 12:08:28 +0800

    week10

---
+++ week10/week10-1.cpp
@@ -0,0 +1,14 @@
+///week 10-1
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int ans =0;
+        int N =mat.size();
+        for (int i =0;i<N;i++){
+            for(int j=0;j<N;j++){
+                if(i==j || i+j==N-1) ans += mat [i][j];
+            }
+        }
+        return ans;
+    }
+};
